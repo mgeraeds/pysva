0da20ac60538b26f49cee825c6515c8e7c24b52e
Marlein Geraeds <mgeraeds@int5.local.snellius.surf.nl>
2023-08-25 00:31:07 +0200

    Changed required data description entries and added more class variables

--- pySVA/sva_core.py
+++ pySVA/sva_core.py
@@ -1,15 +1,23 @@
 __all__ = ['constructorSVA']
 
+# tef.tracer = tef.ds.salt
+# data_description = {
+#     "depth" : "mesh2d_nLayers",
+#     "time" : "time",
+#     "faces" : "mesh2d_nFaces"
+# }
+
 # Do imports
 import xarray as xr
 import xugrid as xu
+import dfm_tools as dfmt
 
 class constructorSVA:
     def __init__(self, input_file, data_description, **kwargs):
 
         if isinstance(input_file, str):
             try:
-                self._read(filename=input_file)
+                self._read(file_name=input_file)
             except (OSError, IOError, RuntimeError):
                 raise IOError("Unable to read file.")
         elif isinstance(input_file, xr.Dataset):
@@ -22,20 +30,27 @@ class constructorSVA:
             raise IOError("Please provide xr.Dataset, xr.DataArray, or a file path to a netCDF.")
 
         self._setup(data=data_description)
+
         self.transport = None
         self.kzz = None
         self.velx = None
         self.vely = None
         self.velz = None
+        self.au = None
+        self.vol = None
+
         self.tracer = None
         self.tracer_variance = None
 
         # hidden
         self._mean_tracer = None
+        self._mean_velx = None
+        self._mean_vely = None
         self._tracer_perturbation = None
 
-    def _read(self, filename, **kwargs):
-        self.ds = xr.open_dataset(filename, use_cftime=True, **kwargs)
+    def _read(self, file_name, **kwargs):
+        # self.ds = xr.open_dataset(file_name, use_cftime=True, **kwargs)
+        self.ds = dfmt.open_partitioned_dataset(file_name, **kwargs)
 
     def _setup(self, data):
         """Iterates over keys in dictionary. Handles 4d-data, if one argument is left empty, dummy dimension will be created.
@@ -50,5 +65,5 @@ class constructorSVA:
 
         self.ds = self.ds.transpose("time",
                                     "depth",
-                                    "lat",
-                                    "lon")
+                                    "nfaces",
+                                    ...)
