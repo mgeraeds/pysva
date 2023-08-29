52c30a3c5cadc50c5def7bc992b8cfd089bbd385
Marlein Geraeds <mgeraeds@int5.local.snellius.surf.nl>
2023-08-29 18:37:28 +0200

    :sparkles: Added helper function for vectorized computations

--- pySVA/sva_helpers.py
+++ pySVA/sva_helpers.py
@@ -1,17 +1,59 @@
 import xugrid as xu
+import xarray as xr
+import warnings
 
-def reconstruct_vector_form(uds, vectors_list):
+def reconstruct_vector_form(constructorSVA, vectors_list, **kwargs):
     
-    # vectors_list = [uds[f'{gridname}_ucx'], uds[f'{gridname}_ucy']] # example with velocities
-
+    # 1. >> Get the basics
+    uds = constructorSVA.ds
     gridname = uds.grid.name
     dimn_cartesian = f'{gridname}_nCartesian_coords'
-    vector_name = f'{gridname}_uc'
+
+    if 'vector_name' in kwargs:
+        vector_name = kwargs['vector_name']
+    else:
+        vector_name = f'{gridname}_uc'
+
+    # vectors_list = [uds[f'{gridname}_ucx'], uds[f'{gridname}_ucy']] # example with velocities
     
-    # > Define the to-be-changed attributes
-    attr_list = {'standard_name' : 'sea_water_velocity', 'long_name':'Flow element center velocity vector'}
-    uds[f'{vector_name}'] = xu.concat(vectors_list, dim=dimn_cartesian).assign_attrs(attr_list)
+    # >> Case 1: if given strings in vector_list, then calculate vector notation based on the constructorSVA.ds dataset
+    if all(isinstance(i, str) for i in vectors_list):
+        
+        # > Change the list to list of xu.core.wrap.UgridDataArray
+        vectors_list_ds = [uds[f'{varname}'] for varname in vectors_list]
+        
+        # > Define the to-be-changed attributes
+        attr_list = {'standard_name' : 'sea_water_velocity', 'long_name':'Flow element center velocity vector'}
+        uds[f'{vector_name}'] = vector_data = xu.concat(vectors_list_ds, dim=dimn_cartesian).assign_attrs(attr_list).rename(vector_name)
 
-    uds = uds.drop_vars(vectors_list)
+        # > Get the names of the original vectors and drop these from the dataset
+        original_vector_names = vectors_list
+        uds.drop_vars(original_vector_names)
+    
+    elif all(isinstance(i, xu.core.wrap.UgridDataArray) for i in vectors_list):
 
-    return
+        # > Define the to-be-changed attributes
+        attr_list = {'standard_name' : 'sea_water_velocity', 'long_name':'Flow element center velocity vector'}
+        uds[f'{vector_name}'] = vector_data = xu.concat(vectors_list, dim=dimn_cartesian).assign_attrs(attr_list).rename(vector_name)
+
+        # > Get the names of the original vectors and drop these from the dataset
+        original_vector_names = [v.name for v in vectors_list]
+        
+        uds.drop_vars(original_vector_names)
+    
+    elif all(isinstance(i, xr.DataArray) for i in vectors_list):
+        
+        # > Define the to-be-changed attributes
+        attr_list = {'standard_name' : 'sea_water_velocity', 'long_name':'Flow element center velocity vector'}
+        uds[f'{vector_name}'] = vector_data = xr.concat(vectors_list, dim=dimn_cartesian).assign_attrs(attr_list).rename(vector_name)
+
+        # > Get the names of the original vectors and drop these from the dataset
+        original_vector_names = [v.name for v in vectors_list]
+        
+        uds.drop_vars(original_vector_names)
+    
+    else:
+        warnings.warn('Type of the provided vectors_list is not recognized. Check your inputs.', UserWarning)
+        return
+
+    return vector_data
