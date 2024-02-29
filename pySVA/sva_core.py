07ef6716e33dc41520aa8fbc6e038dcdd301fc28
meggeraeds <m.e.g.geraeds@tudelft.nl>
2024-02-29 18:13:52 +0100

    :hammer: Removed renaming using data description dictionary

--- pySVA/sva_core.py
+++ pySVA/sva_core.py
@@ -13,7 +13,7 @@ import xugrid as xu
 import dfm_tools as dfmt
 
 class constructorSVA:
-    def __init__(self, input_file, data_description, **kwargs):
+    def __init__(self, input_file,  **kwargs): # removed data description data_description,
 
         if isinstance(input_file, str):
             try:
@@ -26,10 +26,10 @@ class constructorSVA:
             self.ds = input_file
         elif isinstance(input_file, xr.DataArray):
             self.ds = input_file
-        else:
+        # else:
             raise IOError("Please provide xr.Dataset, xu.core.wrap.UgridDataset, xr.DataArray, or a file path to a netCDF.")
 
-        self._setup(data=data_description)
+        # self._setup(data=data_description)
 
         self.transport = None
         self.kzz = None
@@ -49,26 +49,31 @@ class constructorSVA:
         self._mean_tracer = None
         self._mean_velx = None
         self._mean_vely = None
-        self._mean_velu1 = None
+        self._mean_velocity = None
         self._tracer_perturbation = None
+        self._velocity_perturbation = None
         self._unvs = None
 
     def _read(self, file_name, **kwargs):
         # self.ds = xr.open_dataset(file_name, use_cftime=True, **kwargs)
         self.ds = dfmt.open_partitioned_dataset(file_name, **kwargs)
 
-    def _setup(self, data):
-        """Iterates over keys in dictionary. Handles 4d-data, if one argument is left empty, dummy dimension will be created.
-        Args:
-            data (dict): Dictionary that describes geospatial dimensions of the dataset.
-        """
-        for dimension in data.keys():
-            if data[dimension] is None:
-                self.ds = self.ds.expand_dims(dimension)
-            else:
-                self.ds = self.ds.rename({data[dimension]: dimension})
+    # def _setup(self, data):
+    #     """Iterates over keys in dictionary. Handles 4d-data, if one argument is left empty, dummy dimension will be created.
+    #     Args:
+    #         data (dict): Dictionary that describes geospatial dimensions of the dataset.
+    #     """
+    #     for dimension in data.keys():
+    #         if data[dimension] is None:
+    #             self.ds = self.ds.expand_dims(dimension)
+    #         else:
+    #             self.ds = self.ds.rename({data[dimension]: dimension})
 
-        self.ds = self.ds.transpose("time",
-                                    "depth",
-                                    "nfaces",
-                                    ...)
+    #     self.ds = self.ds.transpose("time",
+    #                                 "depth",
+    #                                 "nfaces",
+    #                                 "nedges",
+    #                                 "nnodes",
+    #                                 ...)
+        
+        # removed the nfaces and nedges dimnesion, as they are derived by the xugrid package
