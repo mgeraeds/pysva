3e50ec12f51a202f73917e44cd4390f89a34d642
meggeraeds <m.e.g.geraeds@tudelft.nl>
2024-04-18 19:01:12 +0200

    :bug: Fixed reference to non-existent function

--- pySVA/sva_core.py
+++ pySVA/sva_core.py
@@ -555,7 +555,7 @@ class constructorSVA:
         # > Check if the vertical eddy diffusivity is defined on the edges, and if it is,
         # > interpolate it on the edges
         if self.dimn_edges in kzz.dims:
-            kzz = dfmt.uda_to_edges(kzz)
+            kzz = dfmt.uda_to_faces(kzz)
 
         # > Differentiate the tracer over depth
         dsdz = self.tracer.differentiate(self.dimn_layer)
