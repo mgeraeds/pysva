17f0c937e4352caa77960fc2af802d981bbdb31c
Marlein Geraeds <geraeds@svasek.nl>
2026-03-26 16:17:54 +0100

    chore: version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
       requirements = f.read().splitlines()
 
 setup(name='pysva',
-      version='0.1.0',
+      version='1.0.0',
       author='Marlein Geraeds',
       author_email='m.e.g.geraeds@tudelft.nl',
       packages=find_packages(), 
